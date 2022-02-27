64832cf00909b740b9612c43e85b85c39cdfc0b2
Shinichi Masuda <shinichi.masuda@staff.gu3.co.jp>
2022-02-27 18:08:13 +0900

    add router vuex

---
+++ vue-admin/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+};
--- vue-admin/src/main.ts
+++ vue-admin/src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { router } from './router';
+import { store, key } from './store';
+import App from './App.vue';
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VuesticPlugin);
+app.use(store, key);
+app.use(router);
+
+app.mount('#app');
